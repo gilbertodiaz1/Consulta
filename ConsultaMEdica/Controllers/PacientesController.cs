394f6d13af2596a3744b25b0a688148237bec21b
Jose Diaz <jose.gilbertopinto@gmail.com>
2017-12-14 13:30:56 -0400

    [ADD] se agrego el modelo de la base de dato, se crearon los controller y vistas para seguros y pacientes, se cambio de lugar la carpeta img

---
+++ ConsultaMEdica/Controllers/PacientesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ConsultaMEdica.Models;
+
+namespace ConsultaMEdica.Controllers
+{
+    public class PacientesController : Controller
+    {
+        private ConsultaMedicaEntities db = new ConsultaMedicaEntities();
+
+        // GET: Pacientes
+        public ActionResult Index()
+        {
+            var pacientes = db.Pacientes.Include(p => p.Seguro);
+            return View(pacientes.ToList());
+        }
+
+        // GET: Pacientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Pacientes.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IDSeguro = new SelectList(db.Seguroes, "ID", "Nombre");
+            return View();
+        }
+
+        // POST: Pacientes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Nombre,Apellido,Cedula,FechaNacimiento,Email,Sexo,Edad,IDSeguro")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pacientes.Add(paciente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IDSeguro = new SelectList(db.Seguroes, "ID", "Nombre", paciente.IDSeguro);
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Pacientes.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDSeguro = new SelectList(db.Seguroes, "ID", "Nombre", paciente.IDSeguro);
+            return View(paciente);
+        }
+
+        // POST: Pacientes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombre,Apellido,Cedula,FechaNacimiento,Email,Sexo,Edad,IDSeguro")] Paciente paciente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(paciente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDSeguro = new SelectList(db.Seguroes, "ID", "Nombre", paciente.IDSeguro);
+            return View(paciente);
+        }
+
+        // GET: Pacientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Paciente paciente = db.Pacientes.Find(id);
+            if (paciente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paciente);
+        }
+
+        // POST: Pacientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Paciente paciente = db.Pacientes.Find(id);
+            db.Pacientes.Remove(paciente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ConsultaMEdica/Controllers/SeguroesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ConsultaMEdica.Models;
+
+namespace ConsultaMEdica.Controllers
+{
+    public class SeguroesController : Controller
+    {
+        private ConsultaMedicaEntities db = new ConsultaMedicaEntities();
+
+        // GET: Seguroes
+        public ActionResult Index()
+        {
+            return View(db.Seguroes.ToList());
+        }
+
+        // GET: Seguroes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seguro seguro = db.Seguroes.Find(id);
+            if (seguro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seguro);
+        }
+
+        // GET: Seguroes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Seguroes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Nombre,Descripcion,Direccion,Telefono,Email")] Seguro seguro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Seguroes.Add(seguro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(seguro);
+        }
+
+        // GET: Seguroes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seguro seguro = db.Seguroes.Find(id);
+            if (seguro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seguro);
+        }
+
+        // POST: Seguroes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Nombre,Descripcion,Direccion,Telefono,Email")] Seguro seguro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seguro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(seguro);
+        }
+
+        // GET: Seguroes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seguro seguro = db.Seguroes.Find(id);
+            if (seguro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seguro);
+        }
+
+        // POST: Seguroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seguro seguro = db.Seguroes.Find(id);
+            db.Seguroes.Remove(seguro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
