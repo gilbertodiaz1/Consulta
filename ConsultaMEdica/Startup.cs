e3a1047f721268852e07e278d1202d9eaf6e6d93
Maximiliano Seipio Martins <mseipio@rd.magycorp.com>
2017-12-14 11:09:18 -0400

    Add project files.

---
+++ ConsultaMEdica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConsultaMEdica.Startup))]
+namespace ConsultaMEdica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
